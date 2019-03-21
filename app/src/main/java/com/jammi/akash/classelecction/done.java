3ea506a8ee50fc7ffdc0d9bcf7f8d6ae65521607
Akash JP <jpakash05@gmail.com>
2019-03-21 20:48:31 +0530

    Updated UI 2.0

--- app/src/main/java/com/jammi/akash/classelecction/done.java
+++ app/src/main/java/com/jammi/akash/classelecction/done.java
@@ -1,17 +1,28 @@
 package com.jammi.akash.classelecction;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jammi.akash.classelection.HomeActivity;
+import com.jammi.akash.classelection.MainActivity;
 import com.jammi.akash.classelection.R;
 
 public class done extends AppCompatActivity {
-
+    private static int SPLASH_TIME_OUT =5000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_done);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeintent = new Intent(done.this, HomeActivity.class);
+                startActivity(homeintent);
+
+            }
+        },SPLASH_TIME_OUT);
     }
 //    @override
     public void onBackPressed(){
